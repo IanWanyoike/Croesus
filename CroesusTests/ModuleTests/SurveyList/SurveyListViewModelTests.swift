a7f7eaceed985a2fed953eea42c7b0131e7e5f34
Ian Wanyoike <ian@wanyoike.com>
2020-02-05 11:23:51 +0300

    Update Survey List View Model Tests Add showSurvey test, Begin using test scheduler to create observables for testing. Signed-off-by: Ian Wanyoike <ian@wanyoike.com>

--- CroesusTests/ModuleTests/SurveyList/SurveyListViewModelTests.swift
+++ CroesusTests/ModuleTests/SurveyList/SurveyListViewModelTests.swift
@@ -14,32 +14,61 @@ import Quick
 import Nimble
 
 import RxSwift
+import RxTest
 import RxBlocking
 
 class SurveyListViewModelTests: QuickSpec {
+
+    var disposeBag: DisposeBag?
+
     override func spec() {
-        describe("Load Survey List") {
+        describe("Survey List View Model") {
             var surveyListViewModel: SurveyListViewModel?
+            var testScheduler: TestScheduler?
             beforeEach {
                 let person = Person(id: "1", idNumber: 0)
                 surveyListViewModel = SurveyListViewModel(person: person)
+                testScheduler = TestScheduler(initialClock: 0)
+                self.disposeBag = DisposeBag()
             }
-            it("Fetches And Decodes Survey List") {
-                let disposeBag = DisposeBag()
-                waitUntil { done in
-                    guard let completable = surveyListViewModel?.load() else {
-                        fail("Failed to initialize SurveyListViewModel")
-                        return
-                    }
-                    completable.subscribe { event in
-                        switch event {
-                        case .completed:
-                            done()
-                        case .error(let error):
-                            fail("Failed to load surveys, error: \(error)")
-                        }
-                    }.disposed(by: disposeBag)
+            it("Fetches valid survey list") {
+                guard let viewModel = surveyListViewModel,
+                    let scheduler = testScheduler,
+                    let disposeBag = self.disposeBag else {
+                    return fail()
                 }
+
+                scheduler.createHotObservable([Recorded.next(300, ())])
+                    .bind(to: viewModel.reload)
+                    .disposed(by: disposeBag)
+
+                let result = scheduler.start { viewModel.surveys }
+                expect(result.events.count).to(equal(1))
+
+                guard let surveyViewModel = result.events.first?.value.element?.first else {
+                    return fail()
+                }
+                expect(surveyViewModel.title.value).to(equal("A short review of the Croesus iOS app."))
+            }
+            it("Emits showSurvey when survey is selected") {
+                guard let viewModel = surveyListViewModel,
+                    let scheduler = testScheduler,
+                    let disposeBag = self.disposeBag else {
+                    return fail()
+                }
+
+                let survey = Survey(id: "1", title: "Test Survey", questions: [])
+                let surveyViewModel = SurveyViewModel(survey: survey)
+                let selectSurveyObservable = scheduler.createHotObservable([
+                    Recorded.next(300, surveyViewModel)
+                ])
+
+                selectSurveyObservable
+                    .bind(to: viewModel.selectSurvey)
+                    .disposed(by: disposeBag)
+
+                let result = scheduler.start { viewModel.showSurvey.map { $0.title.value } }
+                expect(result.events).to(equal([Recorded.next(300, survey.title)]))
             }
         }
     }
